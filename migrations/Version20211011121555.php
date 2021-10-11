c888eea8917c9294c356ef96b8eef4ff4363239a
tombsn <bssnthomas@gmail.com>
2021-10-11 14:40:40 +0200

    Commit Thomas B 11/10 14h40

---
+++ migrations/Version20211011121555.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20211011121555 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE lieu ADD ville_id INT NOT NULL');
+        $this->addSql('ALTER TABLE lieu ADD CONSTRAINT FK_2F577D59A73F0036 FOREIGN KEY (ville_id) REFERENCES ville (id)');
+        $this->addSql('CREATE INDEX IDX_2F577D59A73F0036 ON lieu (ville_id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE lieu DROP FOREIGN KEY FK_2F577D59A73F0036');
+        $this->addSql('DROP INDEX IDX_2F577D59A73F0036 ON lieu');
+        $this->addSql('ALTER TABLE lieu DROP ville_id');
+    }
+}
