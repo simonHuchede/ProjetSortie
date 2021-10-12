a8cc962c76d5fcbc01a04237bfbc8147cd0b8470
tombsn <bssnthomas@gmail.com>
2021-10-12 11:00:47 +0200

    Commit Thomas B 12/10 11h

--- src/Form/SortieFormType.php
+++ src/Form/SortieFormType.php
@@ -2,7 +2,9 @@
 
 namespace App\Form;
 
+use App\Entity\Etat;
 use App\Entity\Sortie;
+use App\Entity\Ville;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -19,11 +21,17 @@ class SortieFormType extends AbstractType
             ->add('dateLimiteInscription')
             ->add('nbInscriptionsMax')
             ->add('infosSortie')
-            ->add('etat')
+            /*->add('etat', EntityType::class,[
+                'label'=>'etat : ',
+                'class'=>Etat::class,
+                'choice_label'=>'libelle',
+                'expanded'=>true,
+                "multiple"=>false
+            ])*/
             ->add('lieu')
             //->add('campus',EntityType::class)
             //->add('participants')
-            //->add('organisateur')
+            //->add('organisateur') b
         ;
     }
 
