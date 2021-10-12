bbc3898a9a889b464041b0738bbc4936e47b308e
tombsn <bssnthomas@gmail.com>
2021-10-12 09:07:07 +0200

    Commit Thomas B 11/10 15h

---
+++ src/Controller/SortieController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Sortie;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SortieController extends AbstractController
+{
+
+}
