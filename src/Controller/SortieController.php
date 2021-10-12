0b086cbc0c244a79e46e91c201f3cb62bec189f8
tombsn <bssnthomas@gmail.com>
2021-10-12 17:20:22 +0200

    Commit Thomas B 12/10 17h20

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -5,9 +5,11 @@ namespace App\Controller;
 use App\Entity\Etat;
 use App\Entity\Sortie;
 use App\Form\SortieFormType;
+use App\Repository\EtatRepository;
 use App\Repository\SortieRepository;
 use App\Repository\UtilisateurRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use phpDocumentor\Reflection\Utils;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -22,16 +24,14 @@ class SortieController extends AbstractController
      */
     public function creerSortie(Request $request,
                                  EntityManagerInterface $entityManager,
-                                $id,UtilisateurRepository $utilisateurRepository){
+                                $id,UtilisateurRepository $utilisateurRepository,EtatRepository $repoEtat){
         $utilisateur=$utilisateurRepository->findOneBy(['id'=>$id]);
         $sortie = new Sortie();
         $formulaireSortie= $this->createForm(SortieFormType::class,$sortie);
         $formulaireSortie->handleRequest($request);
-        $etat= new Etat();
-        $etat->setLibelle("créee");
 
         if ($formulaireSortie->isSubmitted() && $formulaireSortie->isValid()){
-
+            $etat = $repoEtat->find(1);
             $sortie->setOrganisateur($utilisateur);
             $sortie->setCampus($utilisateur->getCampus());
             $sortie->setEtat($etat);
@@ -52,6 +52,10 @@ class SortieController extends AbstractController
         );
     }
 
-
-
+    /**
+     * @Route("/detail/{sortie}", name="sortieDetail")
+     */
+    public function detailSortie(Sortie $sortie){
+        return $this->render("sortie/detail.html.twig", compact('sortie'));
+    }
 }
